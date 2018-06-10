f7c56a25a9661d702b529c12e430f56b8a48b67d
computer0101 <deveshpatel7643@gmail.com>
2018-06-10 18:35:27 +0530

    completed restaurant review

---
+++ sw.js
@@ -0,0 +1,42 @@
+self.addEventListener('install', function(event) {
+    console.log('Service worker installed');
+    event.waitUntil(
+        caches.open('rstrnt').then(function(cache) {
+            cache.addAll(['/',
+                '/index.html',
+                '/css/styles.css',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/restaurant.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service worker activated', event);
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
+});
